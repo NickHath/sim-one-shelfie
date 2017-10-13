3268b61de5c6a1c6f2e5b5ad97854e9ec0cdd698
chris-yi <kri5to.yi@gmail.com>
2017-10-13 11:32:24 -0600

    set up massive

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var massive = require("massive");
 var cors = require ("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(bodyParser.json());
-
 app.use(cors());
+
+massive(process.env.CONNECTION_STRING)
+    .then(db => {
+        app.set("db", db)
+    }); 
 
 var port = 3000;
 
