52b93740d76f786db7cd643d120cdaf09437f7ad
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-14 11:43:34 -0600

    home page boilerplate

--- src/App.js
+++ src/App.js
@@ -20,12 +20,25 @@ class App extends Component {
   render() {
     console.log(this.state);
     return (
-      <div className="App">
-        <button onClick={() => this.getTest()}>Get Test</button>
-        <p>Test val: {JSON.stringify(this.state.test)}</p>
+      <div className="container">
+        <div className="nav">
+          <img />
+          <h1>Shelfie</h1>
+        </div>
+        <div className="shelf">Shelf A</div>
+        <div className="shelf">Shelf B</div>
+        <div className="shelf">Shelf C</div>
+        <div className="shelf">Shelf D</div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+{/* <div className="App">
+<button onClick={() => this.getTest()}>Get Test</button>
+<p>Test val: {JSON.stringify(this.state.test)}</p>
+</div> */}
