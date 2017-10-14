8ce526359a81c8a97b2be8219413997a4cf0d53c
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-14 13:38:00 -0600

    shelfs render bins from axios request

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -2,28 +2,32 @@
 // state: [bin1, bin2, bin3] -- objects
 
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Shelf extends Component {
   constructor() {
     super();
     this.state = {
-
+      bins: []
     }
   }
 
+  componentDidMount() {
+    axios.get(`http://localhost:3000/api/${this.props.match.params.shelf}`)
+         .then((res) => this.setState({bins: res.data}))
+  }
+
   render() {
+    let binJSX = this.state.bins.map((bin) => <div className='shelf'>Bin {bin.id}</div>)
     return (
       <div className='container'>
-      <div className='nav'>
-        <div className = 'logo'>
-          <img src='../assets/logo.png'/>
+        <div className='nav'>
+          <div className = 'logo'>
+            <img src='../assets/logo.png'/>
+          </div>
+          <h1>{this.props.match.params.shelf}</h1>
         </div>
-        <h1>Shelf A</h1>
-      </div>
-      <div className='shelf'>Bin 1</div>
-      <div className='shelf'>Bin 2</div>
-      <div className='shelf'>Bin 3</div>
-      <div className='shelf'>Bin 4</div>
+        { binJSX }
     </div>
     )
   }
